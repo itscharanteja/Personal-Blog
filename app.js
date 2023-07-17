026775a7f5c9306e0b65bb6d12a5aff28b0d862f
Charan Teja <68527259+itscharanteja@users.noreply.github.com>
2023-07-17 21:03:08 +0530

    .

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const aboutContent = "aboutContent";
 const contactContent = "contactContent";
 
 const app = express();
-
+ 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
